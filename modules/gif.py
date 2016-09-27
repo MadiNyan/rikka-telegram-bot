c5e251c8afd2feb0fc6f8017a793a758cf2eebac
Madi <madi@madi.so>
2016-09-27 22:58:49 +0300

    gif help improved

--- modules/gif.py
+++ modules/gif.py
@@ -13,15 +13,18 @@ def gif(bot, update, args):
         bot.sendMessage(update.message.chat_id, text=help_gif, reply_to_message_id=update.message.message_id)
         print(datetime.datetime.now(), ">>>", "Gif Help", ">>>", update.message.from_user.username)
     else:
-        for i in folders:
-            if i == args:
-                found = args
-                break
-        dir = "gifs/"+args
-        gifs = [f for f in listdir(dir) if isfile(join(dir, f))]
-        filecount = len(gifs)
-        rand = randint(0, filecount-1)
-        result = list(gifs)[rand]
-        with open(dir+"/"+str(result), "rb") as f:
-            bot.sendDocument(update.message.chat_id, f, reply_to_message_id=update.message.message_id)
-        print(datetime.datetime.now(), ">>>", "Sent gif", ">>>", update.message.from_user.username, ">", result)
+        try:
+            for i in folders:
+                if i == args:
+                    found = args
+                    break
+            dir = "gifs/"+args
+            gifs = [f for f in listdir(dir) if isfile(join(dir, f))]
+            filecount = len(gifs)
+            rand = randint(0, filecount-1)
+            result = list(gifs)[rand]
+            with open(dir+"/"+str(result), "rb") as f:
+                bot.sendDocument(update.message.chat_id, f, reply_to_message_id=update.message.message_id)
+            print(datetime.datetime.now(), ">>>", "Sent gif", ">>>", update.message.from_user.username, ">", result)
+        except:
+            bot.sendMessage(update.message.chat_id, text="No such folder, try /gif help", reply_to_message_id=update.message.message_id)
