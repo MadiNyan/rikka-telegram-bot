bb7c36111628dcf324f2dbd11f0e66b91b3110cf
Madi <madi@madi.so>
2016-10-19 21:21:04 +0300

    reworked post count

--- modules/anime.py
+++ modules/anime.py
@@ -9,18 +9,11 @@ with open('config.yml', 'r') as f:
 
 def get_anime(bot, update, query):
     client = Pybooru('yandere')
-    tags = client.tags_list(query)
-    try:
-        tag_dict = next((item for item in tags if item["name"] == query))
-        tag_count = tag_dict['count']
-        if tag_count >= 100:
-            posts_to_load = 100
-        else:
-            posts_to_load = tag_count
-    except:
-        posts_to_load = 100
-    posts = client.posts_list(query, posts_to_load)
-    random = randint(0, posts_to_load-1)
+    max_posts_to_load = 200
+    posts = client.posts_list(query, max_posts_to_load)
+    post_count = len(posts)
+    print("Post count: ", post_count)
+    random = randint(0, post_count-1)
     image_post = "https://yande.re/post/show/"+str(posts[random]["id"])
     image_url = posts[random]["sample_url"]
     dl = requests.get(image_url)
@@ -41,5 +34,5 @@ def anime(bot, update, args):
     except:
         cap = get_anime(bot, update, "rating:s")
         with open(anime_folder+"anime_temp.jpg","rb") as f:
-            bot.sendPhoto(update.message.chat_id, f, caption="Wrong tag, onii-chan, but here's one random pic:\n"+cap, reply_to_message_id=update.message.message_id)
-        print(datetime.datetime.now(), ">>>", "Sent anime:", input_query, ">>>", update.message.from_user.username)
+            bot.sendPhoto(update.message.chat_id, f, caption="Nothing found, onii-chan, but here's one random pic:\n"+cap, reply_to_message_id=update.message.message_id)
+        print(datetime.datetime.now(), ">>>", "Tag not found:", input_query, "sent random", ">>>", update.message.from_user.username)
