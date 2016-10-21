a7f4f85322fbd3fbc9628568bb5f42751f77cd96
Madi <madi@madi.so>
2016-10-21 17:51:14 +0300

    kappa improvement

--- modules/kappa.py
+++ modules/kappa.py
@@ -1,7 +1,9 @@
 import datetime
+kappas = ["kappa", "каппа"]
 def kappa(bot, update):
-    if "kappa" or "Kappa" or "Каппа" or "каппа" in update.message.text:
-        with open("resources/kappa.webp", "rb") as kappa:
-            bot.sendSticker(update.message.chat_id, kappa, reply_to_message_id=update.message.message_id)
-
-    print(datetime.datetime.now(), ">>>", "Kappa", ">>>", update.message.from_user.username)
+    for word in kappas:
+        if word in update.message.text.lower():
+            with open("resources/kappa.webp", "rb") as kappa:
+                bot.sendSticker(update.message.chat_id, kappa, reply_to_message_id=update.message.message_id)
+            print(datetime.datetime.now(), ">>>", "Kappa", ">>>", update.message.from_user.username)
+            break
