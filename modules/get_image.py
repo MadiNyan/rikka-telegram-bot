bb0c17152de2d895253c06bcda153c0651cdf55f
Madi <madi@madi.so>
2016-10-29 19:51:12 +0300

    new code

--- modules/get_image.py
+++ modules/get_image.py
@@ -1,29 +1,45 @@
 import requests
-import re
 import subprocess
 
 
 def get_image(bot, update, dl_path):
     if update.message.reply_to_message is not None:
-        if "http" in update.message.reply_to_message.text:
-                url = re.findall("http[s]?://\S+?\.(?:jpg|jpeg|png|gif|webp)", update.message.reply_to_message.text)
-                r = requests.get(url[0])
+        # Entities; url, text_link
+        if update.message.reply_to_message.entities is not None:
+            if len(update.message.reply_to_message.entities) != 0:
+                entity = update.message.reply_to_message.entities
+                for i in range(0, len(entity)):
+                    type = entity[i]["type"]
+                    if type == "text_link":
+                        if entity[i]["url"].endswith((".jpg", ".png", ".gif")):
+                            url = entity[i]["url"]
+                    elif type == "url":
+                        offset = entity[i]["offset"]
+                        length = entity[i]["length"]
+                        link = update.message.reply_to_message.text[offset:length+offset]
+                        if link.endswith((".jpg", ".png", ".gif")):
+                            url = link
+                r = requests.get(url)
                 with open(dl_path + "original.jpg", "wb") as f:
                     f.write(r.content)
-        elif update.message.reply_to_message.sticker is not None:
-            sticker = update.message.reply_to_message.sticker.file_id
-            bot.getFile(sticker).download(dl_path + "original.png")
-            stick = "convert  " + dl_path + "original.png -background white -flatten " + dl_path + "original.jpg"
-            subprocess.run(stick, shell=True)
-        elif update.message.reply_to_message.document is not None:
-            if update.message.reply_to_message.document.file_name.endswith((".jpg", ".png")):
-                document = update.message.reply_to_message.document.file_id
-                bot.getFile(document).download(dl_path + "original.jpg")
-            else:
-                raise Exception("Not a valid image")
-        else:
-            photo = update.message.reply_to_message.photo[-1].file_id
-            bot.getFile(photo).download(dl_path + "original.jpg")
+            # Document
+            elif update.message.reply_to_message.document is not None:
+                if update.message.reply_to_message.document.file_name.endswith((".jpg", ".png")):
+                    document = update.message.reply_to_message.document.file_id
+                    bot.getFile(document).download(dl_path + "original.jpg")
+                else:
+                    raise Exception("Not a valid image")
+            # Sticker
+            elif update.message.reply_to_message.sticker is not None:
+                sticker = update.message.reply_to_message.sticker.file_id
+                bot.getFile(sticker).download(dl_path + "original.png")
+                stick = "convert  " + dl_path + "original.png -background white -flatten " + dl_path + "original.jpg"
+                subprocess.run(stick, shell=True)
+            # Photo in reply
+            elif update.message.reply_to_message.photo is not None:
+                photo = update.message.reply_to_message.photo[-1].file_id
+                bot.getFile(photo).download(dl_path + "original.jpg")
+    # Photo w/ caption
     else:
         photo = update.message.photo[-1].file_id
         bot.getFile(photo).download(dl_path + "original.jpg")
