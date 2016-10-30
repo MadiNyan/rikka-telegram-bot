3a39872ce576cb09dda1cc9c7ab6aeba2e10c7e8
Madi <madi@madi.so>
2016-10-30 20:42:03 +0300

    reworked

--- modules/get_image.py
+++ modules/get_image.py
@@ -1,45 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Module courtesy of Slko
 import requests
 import subprocess
+import os.path
+
+
+def extract_url(entity, text):
+    if entity["type"] == "text_link":
+        return entity["url"]
+    elif entity["type"] == "url":
+        offset = entity[i]["offset"]
+        length = entity[i]["length"]
+        return text[offset:offset+length]
+    else:
+        return None
+
+
+def is_image(path):
+    image_extensions = (".jpg", ".png", ".gif")
+    if path is None:
+        return False
+    return path.casefold().endswith(image_extensions)
 
 
 def get_image(bot, update, dl_path):
-    if update.message.reply_to_message is not None:
-        # Entities; url, text_link
-        if update.message.reply_to_message.entities is not None:
-            if len(update.message.reply_to_message.entities) != 0:
-                entity = update.message.reply_to_message.entities
-                for i in range(0, len(entity)):
-                    type = entity[i]["type"]
-                    if type == "text_link":
-                        if entity[i]["url"].endswith((".jpg", ".png", ".gif")):
-                            url = entity[i]["url"]
-                    elif type == "url":
-                        offset = entity[i]["offset"]
-                        length = entity[i]["length"]
-                        link = update.message.reply_to_message.text[offset:length+offset]
-                        if link.endswith((".jpg", ".png", ".gif")):
-                            url = link
-                r = requests.get(url)
-                with open(dl_path + "original.jpg", "wb") as f:
-                    f.write(r.content)
-            # Document
-            elif update.message.reply_to_message.document is not None:
-                if update.message.reply_to_message.document.file_name.endswith((".jpg", ".png")):
-                    document = update.message.reply_to_message.document.file_id
-                    bot.getFile(document).download(dl_path + "original.jpg")
-                else:
-                    raise Exception("Not a valid image")
-            # Sticker
-            elif update.message.reply_to_message.sticker is not None:
-                sticker = update.message.reply_to_message.sticker.file_id
-                bot.getFile(sticker).download(dl_path + "original.png")
-                stick = "convert  " + dl_path + "original.png -background white -flatten " + dl_path + "original.jpg"
-                subprocess.run(stick, shell=True)
-            # Photo in reply
-            elif update.message.reply_to_message.photo is not None:
-                photo = update.message.reply_to_message.photo[-1].file_id
-                bot.getFile(photo).download(dl_path + "original.jpg")
-    # Photo w/ caption
-    else:
-        photo = update.message.photo[-1].file_id
-        bot.getFile(photo).download(dl_path + "original.jpg")
+    output = os.path.join(dl_path, "original.jpg")
+    temp_png = os.path.join(dl_path, "original.png")
+    reply = update.message.reply_to_message
+    if reply is None:
+        return False
+    # Entities; url, text_link
+    if reply.entities is not None:
+        urls = (extract_url(x, reply.text) for x in reply.entities)
+        images = [x for x in urls if is_image(x)]
+        if len(images) > 0:
+            r = requests.get(images[0])  # use only first image url
+            with open(output, "wb") as f:
+                f.write(r.content)
+            return True
+    # Document
+    if reply.document is not None and is_image(reply.document.file_name):
+        bot.getFile(reply.document.file_id).download(output)
+        return True
+    # Sticker
+    if reply.sticker is not None:
+        bot.getFile(reply.sticker.file_id).download(output)
+        stick = "convert  " + temp_png + " -background white -flatten " + output
+        subprocess.run(stick, shell=True)
+        return True
+    # Photo in reply
+    if reply.photo is not None:
+        bot.getFile(reply.photo[-1].file_id).download(output)
+        return True
+    return False
