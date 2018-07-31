75b1478b7b48f1aeb908f54263f53eb7ca9504b2
Anas Idzhaz <madi@madi.so>
2018-07-31 17:09:50 +0300

    logger

--- rikkabot.py
+++ rikkabot.py
@@ -8,6 +8,12 @@ import yaml
 import os
 import re
 
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',	
+                    level=logging.INFO)	
+logger = logging.getLogger(__name__)	
+logging.getLogger("telegram.utils.promise").propagate = False
+
 class Globals:
     def __init__(self, updater, dp, config):
         self.updater = updater
