4feeb94db82b8ac10e97bc6489dbb803da4479d5
Finisto <89861886+Finisto@users.noreply.github.com>
2023-01-22 20:57:40 +0300

    fix gelbooru search
    fixed problem with tags (percenting symbols)
    fixed gelbooru request api

--- modules/anime_search.py
+++ modules/anime_search.py
@@ -6,6 +6,7 @@ from telegram.ext import PrefixHandler
 from telegram import ChatAction
 import requests
 import random
+import urllib.parse
 
 yandere_request_link = "https://yande.re/post.json?limit=100"
 yandere_post_link = "https://yande.re/post/show/"
@@ -38,15 +39,19 @@ def yandere_search(update, context):
 @run_async
 @logging_decorator("gelbooru")
 def gelbooru_search(update, context):
-    query = search(update, context, gelbooru_request_link, gelbooru_post_link)
+    query = search(update, context, gelbooru_request_link, gelbooru_post_link, 'gelbooru')
     return query
 
 
-def search(update, context, request_link, image_link):
+def search(update, context, request_link, image_link, search_place='yandere'):
     update.message.chat.send_action(ChatAction.UPLOAD_PHOTO)
     query = " ".join(context.args)
     try:
-        direct_link, page_link, sample_link = get_image(query, request_link, image_link)
+        if search_place == 'gelbooru':
+            direct_link, page_link, sample_link = get_gelbooru_image(query, request_link, image_link)
+        else:
+            direct_link, page_link, sample_link = get_image(query, request_link, image_link)
+            
     except:
         update.message.reply_text("Sorry, something went wrong!")
         return
@@ -56,6 +61,22 @@ def search(update, context, request_link, image_link):
     msg_text = "[Image]({})".format(direct_link) + "\n" + "[View post]({})".format(page_link)
     update.message.reply_text(msg_text, parse_mode="Markdown")
     return query
+
+
+def get_gelbooru_image(query, request_link, image_link):
+    params = {"tags": query.replace(' ', '+')}  # replace space for plus symbol
+    params_str = urllib.parse.urlencode(params, safe=':+')  # prevent percenting symbols in request
+    response = requests.get(request_link, params=params, proxies=proxies)
+    result_obj = response.json()
+    if not response.text:
+        return None, None
+    if not result_obj:
+        return None, None
+    if result_obj['@attributes']['count'] == 0:  # check if nothing found
+        return None, None
+    post = random.choice(result_obj['post'])  # get random post from list
+    direct_link, page_link, sample_link = post.get("file_url"), image_link+str(post.get("id")), post.get("sample_url")
+    return direct_link, page_link, sample_link
 
 
 def get_image(query, request_link, image_link):
